9f09bee6e849576750b5c95d262de8de51c1de52
SashR <sashinramdhani@gmail.com>
2025-04-02 22:17:22 +0200

    Reword cascade function using surrounding values
    Added basic lose and win effects

--- resources/js/2048/GameMinesweeper.tsx
+++ resources/js/2048/GameMinesweeper.tsx
@@ -12,13 +12,15 @@ class TileData {
 
 interface Data {
     tiles: Array<TileData>,
-    totalMines: number
+    totalMines: number,
+    status: 'won'|'pending'|'lost'
 }
 
 export default function GameMinesweeper () {
     const [data, setData] = useState<Data>({
         tiles: new Array(450).fill(new TileData(2,true)),
-        totalMines: 79
+        totalMines: 99,
+        status: 'pending'
     });
 
     const bombCount = (v:number,i:number, tiles: Array<TileData>) => {
@@ -48,106 +50,49 @@ export default function GameMinesweeper () {
         }
     }
 
-    const tileClick = (pos: number) => {
-        // console.log(pos);
-        cascadeClear(pos, clickTile);
-        clickTile(pos);
-        // setData((data)=>{
-        //     const temp = [...data.tiles];
-        //     temp[pos] = {...temp[pos], clicked: true}
-        //     return {
-        //         ...data,
-        //         tiles: temp
-        //     }
-        // });
-    }
-
-    const clickTile = (pos: number) => {
-        setData((data)=>{
-            const temp = [...data.tiles];
-            temp[pos] = {...temp[pos], clicked: true}
-            return {
-                ...data,
-                tiles: temp
+    const loseEffects = () => {
+        setData(data => {
+            let temp = [...data.tiles];
+            for(let tl of temp) {
+                if(tl.value == -1) tl.clicked = true;
             }
+            return {...data, tiles:temp, status: 'lost'}
         });
     }
 
-    const cascadeClear = (pos:number, callback: (p:number)=>void) => {
-        // AS THERE ARE BUGS CONSIDER REDOING ALGORITHM WITH SURROUDINGS FUNCTION
+    const tileClick = (pos: number) => {
+        if(data.tiles[pos].value === 0) cascadeClear(pos, clickTile);
+        else clickTile(pos);
+    }
 
-        const temp: TileData[] = data.tiles;
-        // If click on an empty tile, clear on tiles that connected to it, check image for reference
-        let [empty, vertical, toReview, borders]: [number[],boolean, number[], number[]] = [[],false, [pos], []];
-        // Clicked on tile 15, it is empty
-
-        // Choose initial direction
-        if((pos>=30 && data.tiles[pos-30].value==0) || (pos<420 && data.tiles[pos+30].value==0)) vertical=true;
-
-        let count = 0; // prevent infinite loop temporarility
-        while(toReview.length > 0 && count<=8){
-            console.log("==========================================================================================================");
-            console.log(`Iteration: ${count}; To Review Array: ${toReview}; vertical: ${vertical}`);
-            count++;
-            const a:number[] = [];
-            for(let v of toReview){
-                if(vertical){
-                    console.log("Moving up and down");
-                    const colStart: number = v%30;
-                    // Traverse to the top
-                    for(let i=v-30; i>=colStart;i-=30){
-                        if(temp[i].value == 0 && ![...toReview,...a,...empty].includes(i)) a.push(i);   // Empty value check => Add to toReview
-                        else {borders.push(i); break;}                                                                     // Stop if hitting a non-zero number
-                    }
-                    // Traverse to the bottom
-                    for(let i=v+30; i<=420+colStart;i+=30){
-                        if(temp[i].value == 0 && ![...toReview,...a,...empty].includes(i)) a.push(i);   // Empty value check => Add to toReview
-                        else {borders.push(i); break;}                                                                    // Stop if hitting a non-zero number
-                    }
+    const clickTile = (pos: number) => {
+        if(data.tiles[pos].value == -1) loseEffects();
+        else {
+            setData((data)=>{
+                const temp = [...data.tiles];
+                temp[pos] = {...temp[pos], clicked: true}
+                return {
+                    ...data,
+                    tiles: temp
                 }
-                if(!vertical) {
-                    console.log("Moving left and right");
-                    const rowStart: number = Math.floor(v/30)*30;
-                    // Traverse to the left
-                    for(let i=v-1; i>=rowStart;i--){
-                        if(temp[i].value == 0 && ![...toReview,...a,...empty].includes(i)) a.push(i);   // Empty value check => Add to toReview
-                        else {borders.push(i); break;}                                                                       // Stop if hitting a non-zero number
-                    }
-                    // Traverse to the right
-                    // console.log(`Row Start: ${rowStart}`);
-                    for(let i=v+1; i<rowStart+30;i++){
-                        // console.log(`To the right pos: ${i}`);
-                        if(temp[i].value == 0 && ![...toReview,...a,...empty].includes(i)) a.push(i);   // Empty value check => Add to toReview
-                        else {borders.push(i); break;}                                                                       // Stop if hitting a non-zero number
-                    }
-                }
-                // toReview = [...toReview.filter(v1=>v1!=v), ...a];                       // Removes v from toReview
-                // empty.push(v);                                                          // Pushes v to empty
-
-            }
-            empty.push(...toReview);                                                    // populate empty with all reviewed values
-            toReview.forEach(v => callback(v));                                         // execute callback for each value reviewed
-            toReview = [...a];                                                          // Load a with newest set of values to review, without already reviewed values
-            vertical = !vertical;                                                   // Switches movement
+            });
         }
+    }
 
-        console.log(`Borders: ${borders}`);
-        let borders2: number[] = [];
-        for(let v of empty) borders2.push(...genSurroundings(v));
-        borders2 = new Array(...(new Set(borders2)));
-        borders2.forEach(callback);
-        // new Array(...(new Set(borders))).map(callback);
-        // Find first non-zero pos to the left and right, generate array
-        // => empty = [], vertical=true; toReview = [14, 15, 16, 17]           ; if the last move is vertical, set vertical to true, else set vertical to false
-        // Loop through this array, going top and down getting empty position, as doing so set clicked property on these positions
-        // => empty = [14, 15, 16, 17]; vertical=false; toReview = [44,74,104,134,164,45,65,46,76,47,77]
-        // Loop through this array, going left and right getting empty position (if it not in empty or toReview), as doing so set clicked property on these positions
-        // => empty = [14, 15, 16, 17, 44,74,104,134,164,45,65,46,76,47,77]; vertical=true; toReview = [78,79,80]
-        // Loop through this array, going up and down getting empty position (if it not in empty or toReview), as doing so set clicked property on these positions
-        // => empty = [14, 15, 16, 17, 44,74,104,134,164,45,65,46,76,47,77,78,79,80]; vertical=false; toReview = [108,138,168,109,139]
-        // Loop through this array, going left and right getting empty position (if it not in empty or toReview), as doing so set clicked property on these positions
-        // => empty = [14, 15, 16, 17, 44,74,104,134,164,45,65,46,76,47,77,78,79,80,108,138,168,109,139]; vertical=true; toReview = []
-        // Since toReview is empty, exit function
+    const cascadeClear = (pos:number,callback:(p:number)=>void) => {
+        let [toBeClicked, temp]:[number[], number[]] =[[], [pos]];
+        while(temp.length>0){
+            let surroundings:number[] = [];
+            // For each element in temp, generate surrounding values
+            for(let v of temp) surroundings.push(...genSurroundings(v));
+            // Filter out values that exist in toBeClicked and duplicates from surrounding values
+            surroundings = new Array(...(new Set(surroundings))).filter(v => !toBeClicked.includes(v));
+            // move original temp value to toBeClicked
+            toBeClicked.push(...temp, ...surroundings.filter(v => data.tiles[v].value > 0));
+            // loop through surrounding values, move non-zero ones to toBeClicked, move zero ones to temp
+            temp = [...surroundings.filter(v => data.tiles[v].value == 0)];
+        }
+        toBeClicked.forEach(callback);
     }
 
     const genSurroundings = (pos:number) => {
@@ -204,16 +149,19 @@ export default function GameMinesweeper () {
                 style={{width: '900px', height:'450px'}}
             >
                 { data.tiles.map((t,i)=> <Tile key={i+1} value={t.value} clicked={t.clicked} position={i} onClick={tileClick} />) }
+                {
+                (data.status == 'won' || data.status == 'lost')  &&
+                <div
+                    className={'transition-all ease-in duration-1000 scale-105 absolute text-7xl italic font-bold w-full h-full flex justify-center items-center ' + (data.status=='won'?'text-green-500':'text-red-500')}
+                    style={{backgroundColor:'rgba(0, 0, 0, 0.61)'}}
+                >
+                    You {data.status}!
+                </div>
+                }
             </div>
 
             <div className='w-96 flex justify-evenly mt-8'>
                 <button onClick={newGame} className='bg-slate-200 text-xl py-2 px-4 rounded-lg w-36'> New game </button>
-                {/* <button
-                    onClick={undo} disabled={undoUsed || !undosAvailable || !count}
-                    className='bg-slate-200 text-xl py-2 px-4 rounded-lg w-36 disabled:bg-slate-500'
-                >
-                    Undo ({undosAvailable})
-                </button> */}
             </div>
         </>
     );
